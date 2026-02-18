6bdd02351c1aecf6e8818cef8031059527c21553
Nick Matthews <linuxpanic1@gmail.com>
2026-02-18 11:01:43 +1100

    Updated

--- W11U22-Devel.sh
+++ W11U22-Devel.sh
@@ -17,7 +17,7 @@ sudo apt update
 #This upgrades the packages
 sudo apt upgrade
 
-#This installs Wine version 10
+#This installs Wine version 11
 sudo apt install --install-recommends winehq-devel
 
-#Subject to GPLv3 License Copyright Nicholas Matthews 2024
+#Subject to GPLv3 License Copyright Nicholas Matthews 2026
